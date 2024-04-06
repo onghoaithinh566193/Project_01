81379ebb48ad17c5d4029aa0cd170ce918eda108
HOAITHINH-HP\THINH <onghoaithinh56@gmail.com>
2024-04-06 12:43:11 +0700

    Update Project_01

--- src/main.java
+++ src/main.java
@@ -1,5 +1,7 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("Good morning!!!");
+        System.out.println("Have a good day!!!");
     }
 }
